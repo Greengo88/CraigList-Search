e7101fb4a2a2d0da58a8c81c283fd8a9ba2dfd78
Geny Isam Hamud Herrera <geny.herrera@gmail.com>
2015-10-27 13:44:52 -0400

    CraigList Search
    CraigList Search is a Google Chrome extension that allows you to quickly
    search and preview the result from CraigList

---
+++ CraigList Search/browser_action/popup.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('searchButton');
+    var queryInput = document.getElementById('query'); 
+  
+    checkPageButton.addEventListener('click', function() {
+        chrome.tabs.getSelected(null, function(tab) {
+            var queryValue = document.getElementById('query').value;    
+            d = document;
+            var f = d.createElement('form');
+            f.action = 'http://craigslist.org/search/sss?sort=rel&query='+queryValue;
+            f.method = 'post';
+            var i = d.createElement('input');
+            i.type = 'hidden';
+            i.name = 'url';
+            i.value = tab.url;
+            f.appendChild(i);
+            d.body.appendChild(f);
+            f.submit();
+        });
+    }, false);
+    
+    queryInput.addEventListener('keydown', function(event) {
+        if (event.keyCode == 13) {
+            checkPageButton.click();
+            return false;
+         }
+    }, false);
+    
+}, false);
